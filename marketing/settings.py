a573dccb78d49c06570d46b130250d3e35c67d45
rockjeon <sangwook27@naver.com>
2021-05-18 02:21:19 +0900

    로그인구현완료

--- marketing/settings.py
+++ marketing/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
